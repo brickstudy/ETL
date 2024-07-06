d886a468c393dd48fceb0dc02f64299450096a9d
robert-min <robertmin522@gmail.com>
2024-07-06 15:34:27 +0900

    delete: Clear folder.

--- brickstudy_ingestion/src/naver_api/NaverAPI.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class NaverAPI:
-    def __init__(self) -> None:
-        self.client_id = os.environ.get('NAVER_API_CLIENT_ID')
-        self.client_secret = os.environ.get('NAVER_API_CLIENT_SECERT')
