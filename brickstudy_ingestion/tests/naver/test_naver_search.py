93e807b98684711eb4bf833633a7852ba09cc296
robert-min <robertmin522@gmail.com>
2024-07-06 15:53:52 +0900

    test: ADD test_naver_search scenario.

---
+++ brickstudy_ingestion/tests/naver/test_naver_search.py
@@ -0,0 +1,26 @@
+
+def test_nave_search_can_connect_client_with_valid():
+    # given : 유효한 네이버 ID, SECRET KEY
+
+    # when : Naver news api 요청
+
+    # then : json 데이터 확인
+    pass
+
+
+def test_naver_search_cannot_connect_client_with_invalid_id():
+    # given : 잘못된 네이버 ID
+
+    # when : Naver news api 요청
+
+    # then : 에러메시지
+    pass
+
+
+def test_naver_search_cannot_connect_client_with_invalid_secret_key():
+    # given : 잘못된 네이버 ID
+
+    # when : Naver news api 요청
+
+    # then : 에러메시지
+    pass
