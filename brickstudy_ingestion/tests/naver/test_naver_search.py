480294d7590250b5a321332c7ba34589cd1c4a03
seoyeong200 <youngseogimkk@gmail.com>
2024-07-23 22:28:06 +0900

    fix : s3 업로드 테스트코드 src로 대체

--- brickstudy_ingestion/tests/naver/test_naver_search.py
+++ brickstudy_ingestion/tests/naver/test_naver_search.py
@@ -3,6 +3,7 @@ from src.common.errorcode import Naver
 from src.common.exception import ExtractError
 from src.naver.naver_search import NaverSearch
 from src.common.aws_s3_mime import EXTENSION_TO_MIME
+from src.common.s3_uploader import S3Uploader
 
 # Mock
 PLATFROM = "news"
@@ -64,52 +65,41 @@ def test_naver_search_cannot_connect_client_with_invalid_secret_key():
 
 
 def test_naver_search_can_connect_to_aws():
-    import os, json
+    import json
     from datetime import datetime
-    import boto3
 
     # given : valid s3 client id, password
-    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-
     # when : request that trying to connect aws
     # then : task success
     data = {'lastBuildDate': 'Sun, 21 Jul 2024 21:14:41 +0900', 'total': 5414727, 'start': 1, 'display': 1, 'items': [{'title': '[단독]한전, 건설지역서 10년간 선심성 식사·<b>여행</b> 등에 25억 이상 썼다', 'originallink': 'https://www.khan.co.kr/economy/economy-general/article/202407211426001', 'link': 'https://n.news.naver.com/mnews/article/032/0003309729?sid=101', 'description': '대부분 주민에게 식사와 기념품, <b>여행</b>을 제공하는 데 사용됐다. 한 끼에 850만원이 넘는 금액이 결제되거나, 하루 견학에 쓰인 버스 임차비로 1300만원이 지출된 사례도 있었다. 견학에 참여했던 한 주민은 “스무 명... ', 'pubDate': 'Sun, 21 Jul 2024 14:27:00 +0900'}]}
     json_data = json.dumps(data)
 
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=aws_access_key_id, 
-        aws_secret_access_key=aws_secret_access_key
-    )
-    bucket_name = 'brickstudy'
+    s3uploader = S3Uploader()
+
     id = datetime.today().strftime("%Y-%m-%d %H:%M:%S.%f")
-    file_destination=f'travel/bronze/naverAPI/{PLATFROM}/'
-    file_name=f'{PLATFROM}_{QUERY}_{id}'
-    file_key=file_destination+file_name
- 
-    s3.put_object(
-        Bucket=bucket_name, 
-        Key=file_key, 
-        Body=json_data,
-        ContentType='application/json'
+    s3uploader.write_s3(
+        bucket_name='brickstudy',
+        file_key=f'travel/bronze/naverAPI/{PLATFROM}/{PLATFROM}_{QUERY}_{id}',
+        data_type='json',
+        data=json_data
     )
 
-    response = s3.get_object(
-        Bucket=bucket_name,
-        Key=file_key
+    response = s3uploader.s3_client.get_object(
+        Bucket='brickstudy',
+        Key=f'travel/bronze/naverAPI/{PLATFROM}/{PLATFROM}_{QUERY}_{id}'
     )
     content = response['Body'].read().decode('utf-8')
     json_content = json.loads(content)
 
     assert json_content == data
 
-
+#TODO s3_uploader, get_mimetype으로 대체
 def test_s3_uploader_mime_get_right_content_type():
     # given : 올바른 data type
     extension = "json"
     # when : put_object()의 ContentType 옵션에 들어갈 파일 타입 받아오기
     # then : 알맞게 매핑된 ContentType
+    
     content_type = EXTENSION_TO_MIME.get(extension) 
     assert content_type == 'application/json'
 
