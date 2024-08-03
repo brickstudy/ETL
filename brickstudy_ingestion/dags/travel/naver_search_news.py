984348d6c602704a39035589c1950bc07d1c1f0d
seoyeong200 <youngseogimkk@gmail.com>
2024-08-04 02:17:54 +0900

    fix : airflow dag bug

--- brickstudy_ingestion/dags/travel/naver_search_news.py
+++ brickstudy_ingestion/dags/travel/naver_search_news.py
@@ -1,4 +1,5 @@
-from datetime import timedelta, datetime
+from datetime import timedelta
+import boto3
 
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
@@ -7,10 +8,14 @@ from airflow.operators.python import PythonOperator
 from src.naver.naver_search import NaverSearch
 from src.common.aws.s3_uploader import S3Uploader
 from dags.config import (
-    S3_BUCKET_NAME,
-    S3_BRONZE_BASE_PATH
+    NAVER_API_CLIENT_ID,
+    NAVER_API_CLIENT_SECERT,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME
 )
 
+DAG_ID = 'bronze_travel_naverapi'
 TARGET_PLATFORM = "news"
 QUERY = "여행"
 
@@ -31,7 +36,11 @@ def fetch_and_store():
 
 
 def request_naver_api():
-    client = NaverSearch(TARGET_PLATFORM)
+    client = NaverSearch(
+        client_id=NAVER_API_CLIENT_ID,
+        client_secret=NAVER_API_CLIENT_SECERT,
+        target_platform=TARGET_PLATFORM
+    )
     return client.request_with_keyword(
         query=QUERY,
         display=100
@@ -39,18 +48,23 @@ def request_naver_api():
 
 
 def upload_to_s3(data):
-    timestamp = datetime.strftime(data["lastBuildDate"], "%a, %d %b %Y %H:%M:%S %z").timestamp()
-    s3_uploader = S3Uploader()
+    timestamp = data["lastBuildDate"]
+    s3_client = boto3.client(
+        "s3", 
+        aws_access_key_id=AWS_ACCESS_KEY_ID, 
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+    s3_uploader = S3Uploader(s3_client)
     s3_uploader.write_s3(
         bucket_name=S3_BUCKET_NAME,
-        file_key=f"{S3_BRONZE_BASE_PATH}/travel/naverapi/{TARGET_PLATFORM}/{timestamp}",
+        file_key=f"{DAG_ID.replace('_', '/')}/{TARGET_PLATFORM}_{timestamp}",
         data_type='json',
         data=data["items"][0]
     )
 
 
 with DAG(
-    dag_id='naver_search_api_news',
+    dag_id=DAG_ID,
     default_args=default_args,
     description='sample dag for fetching data from naver news via NaverAPI',
     schedule_interval='@daily',
