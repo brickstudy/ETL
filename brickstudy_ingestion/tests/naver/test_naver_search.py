e1c95d62a6fe5399e537931fa43e267caa387e90
seoyeong200 <youngseogimkk@gmail.com>
2024-08-04 01:14:45 +0900

    fix : init instance with id&secret, exception

--- brickstudy_ingestion/tests/naver/test_naver_search.py
+++ brickstudy_ingestion/tests/naver/test_naver_search.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from src.common.errorcode import Naver
 from src.common.exception import ExtractError
 from src.naver.naver_search import NaverSearch
@@ -13,12 +14,21 @@ START = 1   # 검색 시작 위치(기본값: 1, 최댓값: 1000)
 SORT = "sim"    # 검색 결과 정렬 방법 - sim: 정확도순으로 내림차순 정렬(기본값) - date: 날짜순으로 내림차순 정렬
 
 
-def test_nave_search_can_connect_client_with_valid():
+@pytest.fixture
+def naver_api_client():
+    return NaverSearch(
+        client_id=os.getenv('NAVER_API_CLIENT_ID'),
+        client_secret=os.getenv('NAVER_API_CLIENT_SECERT'),
+        target_platform=PLATFROM,
+        resp_format=RESPONSE_FORMAT
+    )
+
+
+def test_nave_search_can_connect_client_with_valid(naver_api_client):
     # given : 유효한 네이버 ID, SECRET KEY
 
     # when : Naver news api 요청
-    client = NaverSearch(PLATFROM, RESPONSE_FORMAT)
-    result = client.request_with_keyword(QUERY, DISPLAY, START, SORT)
+    result = naver_api_client.request_with_keyword(QUERY, DISPLAY, START, SORT)
 
     # then : json 데이터 확인
     assert result["start"] == 1
@@ -31,43 +41,40 @@ def test_nave_search_can_connect_client_with_valid():
     assert result["items"][0]["pubDate"]
 
 
-def test_naver_search_cannot_connect_client_with_invalid_id():
+def test_naver_search_cannot_connect_client_with_invalid_id(naver_api_client):
     # given : 잘못된 네이버 ID
     wrong_id = "wrong_id"
 
     # when : Naver news api 요청
     # then : errorcode
     with pytest.raises(ExtractError):
-        client = NaverSearch(PLATFROM, RESPONSE_FORMAT)
-        client.headers["X-Naver-Client-Id"] = wrong_id
-        result = client.request_with_keyword(QUERY, DISPLAY, START, SORT)
+        naver_api_client.headers["X-Naver-Client-Id"] = wrong_id
+        result = naver_api_client.request_with_keyword(QUERY, DISPLAY, START, SORT)
 
         assert result["message"] == Naver.AuthError.value["message"]
         # assert result["log"]["errorMessage"] == 'NID AUTH Result Invalid (1000) : Authentication failed. (인증에 실패했습니다.)'
         # assert result["log"]["errorCode"] == '024'
 
 
-def test_naver_search_cannot_connect_client_with_invalid_secret_key():
+def test_naver_search_cannot_connect_client_with_invalid_secret_key(naver_api_client):
     # given : 잘못된 네이버 ID
     wrong_secret = "wrong_secret"
 
     # when : Naver news api 요청
     # then : errorcode
     with pytest.raises(ExtractError):
-        client = NaverSearch(PLATFROM, RESPONSE_FORMAT)
-        client.headers["X-Naver-Client-Secret"] = wrong_secret
-        result = client.request_with_keyword(QUERY, DISPLAY, START, SORT)
+        naver_api_client.headers["X-Naver-Client-Secret"] = wrong_secret
+        result = naver_api_client.request_with_keyword(QUERY, DISPLAY, START, SORT)
 
         assert result["message"] == Naver.AuthError.value["message"]
         # assert result["log"]["errorMessage"] == 'NID AUTH Result Invalid (28) : Authentication failed. (인증에 실패했습니다.)'
         # assert result["log"]["errorCode"] == '024'
 
 
-def test_naver_search_exceed_daily_request_quota():
+def test_naver_search_exceed_daily_request_quota(naver_api_client):
     # given : 네이버 api request 할당량 초과 or 1초에 너무 많은 request
     # then : errorcode 429
     with pytest.raises(ExtractError):
-        client = NaverSearch(PLATFROM, RESPONSE_FORMAT)
         for _ in range(25000):
-            result = client.request_with_keyword(QUERY)
+            result = naver_api_client.request_with_keyword(QUERY)
         assert result["message"] == Naver.LimitExceedError.value["message"]
