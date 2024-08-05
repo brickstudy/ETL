af25a01d23eef5ef6008b449be087c416d3cbf88
seoyeong200 <youngseogimkk@gmail.com>
2024-08-05 16:50:36 +0900

    test : 브랜드관 url 수집, 쿼리 키워드 추가

--- brickstudy_ingestion/tests/scrapper/test_oliveyoung.py
+++ brickstudy_ingestion/tests/scrapper/test_oliveyoung.py
@@ -22,8 +22,34 @@ def test_get_brand_in_each_category():
     assert brand_info is not None
 
 
+@pytest.mark.skip(reason="tested")
 def test_generate_query_keyword():
     brand_dict = get_brand_in_each_category(category_url)
     brand_dict = generate_query_keyword(brand_dict)
-
     assert brand_dict is not None
+
+
+def test_generate_brand_shop_url():
+    from src.scrapper import get_soup
+    
+    brand_dict = generate_query_keyword(
+        get_brand_in_each_category(category_url)
+    )
+
+    total_brand_list_soup = get_soup("https://www.oliveyoung.co.kr/store/main/getBrandList.do")
+    brand_base_url = "https://www.oliveyoung.co.kr/store/display/getBrandShopDetail.do?onlBrndCd="
+    code_name = {}
+    for a_tag in total_brand_list_soup.find_all('a'):
+        brand_code = a_tag.get('data-ref-onlbrndcd')
+        if brand_code:
+            brand_name = a_tag.text
+            if brand_name in brand_dict.keys():  # Kor brand name
+                brand_dict[brand_name].brand_shop_detail_url = brand_base_url + brand_code
+                code_name[brand_code] = brand_name
+            else:                                # Eng brand name
+                try:
+                    kor_brand_name = code_name[brand_code]
+                    brand_dict[kor_brand_name].query_keyword.append(brand_name)
+                finally:
+                    continue
+    print(brand_dict)
