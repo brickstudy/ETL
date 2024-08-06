9014989696812b9ab2659814ffcf7dba15c74e04
seoyeong200 <youngseogimkk@gmail.com>
2024-08-06 15:57:22 +0900

    add : add request header, adjust request freuqency

--- brickstudy_ingestion/src/scrapper/__init__.py
+++ brickstudy_ingestion/src/scrapper/__init__.py
@@ -1,9 +1,28 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
+import random
+import time
+
+from src.common.exception import ExtractError
 
 
 def get_soup(url: str = None):
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    return soup
+    user_agent_lst = ['Googlebot', 'Yeti', 'Daumoa', 'Twitterbot']
+    user_agent = user_agent_lst[random.randint(0, len(user_agent_lst) - 1)]
+    headers = {'User-Agent': user_agent}
+
+    try:
+        page = urlopen(url, headers=headers)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+    except (HTTPError, URLError) as e:
+        ExtractError(
+            code=000,
+            message=f"**{url}** open error",
+            log=e
+        )
+        time.sleep(5)
+    else:
+        time.sleep(random.randrange(0, 3))
+        return soup
