396d4dd099a20f2dc94e4c303d255a6efaeeaab1
robert-min <robertmin522@gmail.com>
2024-08-12 11:51:25 +0900

    feature: ADD airflow connection auto setting.

--- brickstudy_ingestion/dags/common/set_env_config.py
+++ brickstudy_ingestion/dags/common/set_env_config.py
@@ -1,11 +1,20 @@
 from airflow import DAG
-from airflow.models import Variable
+from airflow.models import Variable, Connection
 from airflow.utils.dates import days_ago
 from airflow.providers.mysql.operators.mysql import MySqlOperator
 from airflow.operators.python import PythonOperator
-
+from airflow.utils.db import provide_session
 
 # =========================================
+"""
+Description:
+수동 설정 필요한 connections
+    - MYSQL_CONN_URL
+수동 설정 필요한 variables
+    - AWS_ACCESS_KEY_ID
+    - AWS_SECRET_ACCESS_KEY
+"""
+
 # Set aiflow setting
 default_args = {
     'owner': 'brickstudy',
@@ -14,9 +23,31 @@ default_args = {
 # =========================================
 
 
-def set_env_vars(**context):
-    """ MySQL에서 가져온 데이터를 환경 변수로 설정하는 함수 """
-    query_results = context['task_instance'].xcom_pull(task_ids='get_env_vars')
+@provide_session
+def set_airflow_env_var(session=None, **context):
+    """ airflow 테이블에서 가져온 데이터를 Ariflow Connection으로 설정하는 함수 """
+    query_results = context['task_instance'].xcom_pull(task_ids='get_airflow_env_vars')
+
+    if not query_results:
+        raise ValueError("No data received from MySQL query")
+
+    for id_, type_, host_ in query_results:
+        conn = Connection(
+            conn_id=id_,
+            conn_type=type_,
+            host=host_
+        )
+        existing_conn = session.query(Connection).filter(Connection.conn_id == conn.conn_id).first()
+        if existing_conn:
+            session.delete(existing_conn)
+            session.commit()
+        session.add(conn)
+        session.commit()
+
+
+def set_media_env_var(**context):
+    """ media 테이블에서 가져온 데이터를 Airflow variables로 설정하는 함수 """
+    query_results = context['task_instance'].xcom_pull(task_ids='get_media_env_vars')
 
     # set env variables
     for source_, client_id, client_pw in query_results:
@@ -32,22 +63,36 @@ def set_env_vars(**context):
 with DAG(
     dag_id='0_set_airflow_env_variable',
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval='0 0,12 * * *',
     catchup=False,
 ) as dag:
+    # Run airflow table query
+    get_airflow_env_vars = MySqlOperator(
+        task_id='get_airflow_env_vars',
+        mysql_conn_id='MYSQL_CONN_URL',
+        sql='SELECT id_, type_, host_ FROM brickas.airflow;',   # TODO: connection 늘어나면 수정
+    )
 
-    # Run mysql query
-    get_env_vars = MySqlOperator(
-        task_id='get_env_vars',
+    # Set airflow env variables
+    set_airflow_env_var = PythonOperator(
+        task_id='set_airflow_env_var',
+        python_callable=set_airflow_env_var,
+        provide_context=True,
+    )
+
+    # Run media table query
+    get_media_env_vars = MySqlOperator(
+        task_id='get_media_env_vars',
         mysql_conn_id='MYSQL_CONN_URL',
         sql='SELECT source_, client_id, client_pw FROM brickas.media;',
     )
 
-    # Set env variables
-    set_env_var = PythonOperator(
-        task_id='set_env_var',
-        python_callable=set_env_vars,
+    # Set media env variables
+    set_media_env_var = PythonOperator(
+        task_id='set_media_env_var',
+        python_callable=set_media_env_var,
         provide_context=True,
     )
 
-    get_env_vars >> set_env_var
+    get_airflow_env_vars >> set_airflow_env_var
+    get_media_env_vars >> set_media_env_var
--- brickstudy_ingestion/dags/utils/config.py
+++ brickstudy_ingestion/dags/utils/config.py
@@ -2,7 +2,7 @@ import os
 from airflow.models import Variable
 
 
-#TODO : 변수 아래처럼 직접 입력 없이 airflow 메타데이터에서 바로 가져올 수 있도록!!
+# TODO : 변수 아래처럼 직접 입력 없이 airflow 메타데이터에서 바로 가져올 수 있도록!!
 # SET VARIABLES
 def set_env_variables():
     ALL_ENV_VARIABLES = [
@@ -17,14 +17,3 @@ def set_env_variables():
     ]
     for ENV_VARIABLE in ALL_ENV_VARIABLES:
         os.environ[ENV_VARIABLE] = Variable.get(ENV_VARIABLE, "")
-
-# ALL_ENV_VARIABLES = [
-#     # AWS
-#     "AWS_ACCESS_KEY_ID",
-#     "AWS_SECRET_ACCESS_KEY"
-#     # MYSQL
-#     "NEWS_API_CLIENT_ID"
-# ]
-
-# for ENV_VARIABLE in ALL_ENV_VARIABLES:
-#     os.environ[ENV_VARIABLE] = Variable.get(ENV_VARIABLE, "")
