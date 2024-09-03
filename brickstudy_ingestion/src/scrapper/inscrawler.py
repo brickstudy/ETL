f299ae2384fe989b28ec65f85cca1026b8ff68ed
seoyeong200 <youngseogimkk@gmail.com>
2024-09-03 19:27:11 +0900

    add : materialize method in inscrawler

--- brickstudy_ingestion/src/scrapper/inscrawler.py
+++ brickstudy_ingestion/src/scrapper/inscrawler.py
@@ -1,4 +1,3 @@
-import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,8 +9,9 @@ from src.scrapper.models import inst_generator
 
 class InsCrawler:
     def __init__(self):
-        #TODO base_dir 받는 부분 수정
-        self.base_dir = '/Users/seoyeongkim/Documents/ETL/brickstudy_ingestion/src/scrapper'
+        # TODO proj_path(실행환경의 project 절대경로) 받는 부분 수정  환경변수 설정 필요
+        proj_path = '/Users/seoyeongkim/Documents/ETL/brickstudy_ingestion'
+        self.base_path = f'{proj_path}/src/scrapper'
         self.user_id, self.password, self.keywords, self.iter = self.load_config()
         self.data = defaultdict(inst_generator)
         self.driver = webdriver.Chrome()
@@ -38,3 +38,14 @@ class InsCrawler:
         self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
         self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div').click()
         time.sleep(80)
+
+    def materialize(self):
+        """
+        self.data to csv file
+        """
+        from src.scrapper.utils import current_datetime_getter
+        import csv
+
+        with open(f"{self.base_path}/insdata_{current_datetime_getter()}.csv", 'w') as f:
+            w = csv.writer(f)
+            w.writerow(self.data.values())
