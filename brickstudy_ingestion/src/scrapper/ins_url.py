aba10541cf83b2cba5036c211f8ce3c5621fc2ea
seoyeong200 <youngseogimkk@gmail.com>
2024-09-03 17:48:11 +0900

    ADD : 인스타그램 url 수집 모듈 추가

---
+++ brickstudy_ingestion/src/scrapper/ins_url.py
@@ -0,0 +1,66 @@
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+import urllib
+import re
+
+from src.scrapper.inscrawler import InsCrawler
+
+
+class InsURLCrawler(InsCrawler):
+    def __init__(self):
+        super().__init__()
+
+    def get_urls(self, keyword: str = None):
+        if keyword is not None:              # execute with given keyword
+            self._fetch_url_data(keyword)
+        else:                                # execute with entire keywords
+            for keyword in self.keywords:
+                self._fetch_url_data(keyword)
+
+    def _fetch_url_data(self, keyword):
+        word = urllib.parse.quote(keyword)
+        word_url = f'https://www.instagram.com/explore/tags/{word}/'
+        self.driver.get(word_url)
+
+        try:
+            time.sleep(5)
+            js = 'window.scrollBy(0,1000)'
+            self.driver.execute_script(js)
+            html = self.driver.page_source
+            soup = BeautifulSoup(html, 'lxml')
+
+            divimg = soup.find_all('img', {'class': 'x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3'})
+            if not divimg:
+                print('이미지를 찾을 수 없습니다.')
+                raise Exception
+
+            for div in divimg:
+                content = div.get('alt')
+                if not content:
+                    print('내용이 없습니다.')
+                    continue
+
+                a = div.find_parent('a')
+                if a is None:
+                    print('게시물 링크가 잘못되었습니다.')
+                    continue
+                urlto = a.get('href')
+                if urlto is None:
+                    print('게시물 링크가 없습니다.')
+                    continue
+                totalurl = 'https://www.instagram.com' + urlto
+                self.data[urlto].brand = keyword
+                self.data[urlto].post_url = totalurl
+
+                modified_content = re.sub(r'\s*\n\s*', ' ', content)
+                self.data[urlto].full_text = modified_content
+
+            print(f'페이지 {keyword}에서 데이터를 가져오는 중...')
+            time.sleep(5)
+
+        except Exception as e:
+            print(e)
+            print('오류 발생')
+
+        print(f'키워드 {keyword}의 URL 정보 수집 완료.')
--- brickstudy_ingestion/src/scrapper/inscrawler.py
+++ brickstudy_ingestion/src/scrapper/inscrawler.py
@@ -1,43 +1,40 @@
 import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+from collections import defaultdict
 
-from src.common.exception import RetryException
-
-from browser import Browser
-from utils import retry
+from src.scrapper.models import inst_generator
 
 
-class InsCrawler():
-    URL = "https://www.instagram.com"
-    RETRY_LIMIT = 10
-
-    def __init__(self, has_screen=False):
-        super(InsCrawler, self).__init__()
-        self.browser = Browser(has_screen)
-        self.page_height = 0
+class InsCrawler:
+    def __init__(self):
+        #TODO base_dir 받는 부분 수정
+        self.base_dir = '/Users/seoyeongkim/Documents/ETL/brickstudy_ingestion/src/scrapper'
+        self.user_id, self.password, self.keywords, self.iter = self.load_config()
+        self.data = defaultdict(inst_generator)
+        self.driver = webdriver.Chrome()
         self.login()
 
+    def load_config(self):
+        with open(f'{self.base_path}/config.json', 'r', encoding='utf-8') as f:
+            config = json.load(f)
+
+        username = config['login']['username']
+        password = config['login']['password']
+        keywords = config['keywords']
+        iter = config['iter']
+
+        return username, password, keywords, iter
+
     def login(self):
-        browser = self.browser
-        url = "%s/accounts/login/" % (InsCrawler.URL)
-        browser.get(url)
-        u_input = browser.find_one('input[name="username"]')
-        u_input.send_keys(os.getenv('INSTAGRAM_ID'))
-        p_input = browser.find_one('input[name="password"]')
-        p_input.send_keys(os.getenv('INSTAGRAM_PWD'))
-
-        login_btn = browser.find_one('button[type="submit"]')
-        login_btn.click()
-
-        @retry()
-        def check_login():
-            if browser.find_one('input[name="username"]'):
-                raise RetryException()
-
-        check_login()
-
-    def get_latest_posts_by_tag(self, tag, num):
-        tag = 'enhypen'
-        url = f"{InsCrawler.URL}/explore/search/keyword/?q=%23{tag}"
-        self.browser.get(url)
-        self.browser.scroll_down()
-        #TODO 게시물 클릭, 컨텐츠 가져오기
+        # Instagram 접속 및 로그인
+        url = 'https://www.instagram.com/'
+        self.driver.get(url)
+        time.sleep(6)
+        user = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+        user.send_keys(self.user_id)
+        self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
+        self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div').click()
+        time.sleep(80)
