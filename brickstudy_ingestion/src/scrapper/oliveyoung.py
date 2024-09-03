b763d17210a688a50209150b326cbbb8e1596d1e
seoyeong200 <youngseogimkk@gmail.com>
2024-09-03 16:02:47 +0900

    fix : bug- typo

--- brickstudy_ingestion/src/scrapper/oliveyoung.py
+++ brickstudy_ingestion/src/scrapper/oliveyoung.py
@@ -101,7 +101,7 @@ class Brand:
         """
         for brand in self.brand_metadata.keys():
             brand_url = self.brand_metadata[brand].brand_shop_detail_url
-            brand_url_soup = get_soup(brand_url)==----------0-0
+            brand_url_soup = get_soup(brand_url)
             if brand_url_soup is None:
                 continue
             item_dic = {}
