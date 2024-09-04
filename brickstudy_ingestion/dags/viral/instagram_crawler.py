f7a56ea9953276b28e1adf42096053f97e446981
seoyeong200 <youngseogimkk@gmail.com>
2024-09-04 12:59:54 +0900

    fix : instagram dag 브랜드리스트 받는 부분 task 분리

--- brickstudy_ingestion/dags/viral/instagram_crawler.py
+++ brickstudy_ingestion/dags/viral/instagram_crawler.py
@@ -2,8 +2,8 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.decorators import task
-from airflow.operators.python import PythonVirtualenvOperator
+from airflow.operators.python import PythonVirtualenvOperator, PythonOperator
+from airflow.models import Variable
 
 from src.scrapper.brand_name_getter import get_brand_list_fr_s3
 
@@ -23,16 +23,23 @@ default_args = {
 # =========================================
 
 
-def entrypoint():
+def get_brand_list():
+    import os
+    for ENV_VARIABLE in ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY']:
+        os.environ[ENV_VARIABLE] = Variable.get(ENV_VARIABLE, "")
+    return get_brand_list_fr_s3()
+
+
+def instagram_crawling(brand_lst, id, pwd):
+    import os
     import logging
-    import multiprocess
     from src.common.kafka.utils import Kafka
     from src.scrapper.inscrawler import InsCrawler
     from src.scrapper.ins_url import InsURLCrawler
     from src.scrapper.ins_data import InsDataCrawler
 
-    brand_lst = get_brand_list_fr_s3
-    CONCURRENCY_LEVEL = multiprocess.cpu_count()
+    os.environ['INSTAGRAM_CLIENT_ID'] = id
+    os.environ['INSTAGRAM_CLIENT_PASSWORD'] = pwd
 
     def crawl_instagram(keywords: tuple):
         crawler = InsURLCrawler(InsCrawler(keywords=keywords)).get_urls()
@@ -45,11 +52,11 @@ def entrypoint():
 
     try:
         producer = Kafka()
-        with multiprocess.Pool(CONCURRENCY_LEVEL) as p:
-            p.map(crawl_instagram, brand_lst)
+        crawl_instagram(brand_lst)
     except Exception as e:
         logging.error("***entrypoint error***", e)
         raise
+
 
 with DAG(
     dag_id=DAG_ID,
@@ -57,13 +64,26 @@ with DAG(
     schedule_interval='@daily',
     catchup=False
 ):
-    t_crawl_ins = PythonVirtualenvOperator(
+    t1 = PythonOperator(
+        task_id='get_brand_list_from_s3',
+        python_callable=get_brand_list
+    )
+
+    t2 = PythonVirtualenvOperator(
         task_id='crawl_instagram_based_on_keyword',
+        system_site_packages=False,
+        op_kwargs={
+            'brand_lst': "{{ ti.xcom_pull(task_ids='get_brand_list_from_s3') }}",
+            'id': Variable.get('INSTAGRAM_CLIENT_ID'),
+            'pwd': Variable.get('INSTAGRAM_CLIENT_PASSWORD')
+        },
         python_version='3.10',
         system_site_packages=False,
         requirements=['selenium==4.24.0', 'webdriver-manager==4.0.2',
                       'bs4==0.0.2', 'beautifulsoup4==4.12.3',
                       'lxml==5.3.0', 'pytz==2024.1',
                       "python-dotenv==0.19.0", "multiprocess", "kafka-python"],
-        python_callable=entrypoint
+        python_callable=instagram_crawling
     )
+
+    t1 >> t2
